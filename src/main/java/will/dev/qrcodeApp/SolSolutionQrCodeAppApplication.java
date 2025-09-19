077211d1ed2774dcd5ed47c85eb860412f38c6b2
williamndongmo88 <williamndongmo88@gmail.com>
2025-09-19 17:45:52 +0200

    Add railway deployment config

--- src/main/java/will/dev/qrcodeApp/SolSolutionQrCodeAppApplication.java
+++ src/main/java/will/dev/qrcodeApp/SolSolutionQrCodeAppApplication.java
@@ -4,11 +4,18 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SolSolutionQrCodeAppApplication {
+
+    private final Environment environment;
+
+    public SolSolutionQrCodeAppApplication(Environment environment) {
+        this.environment = environment;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SolSolutionQrCodeAppApplication.class, args);
