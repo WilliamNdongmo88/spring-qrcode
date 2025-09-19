06a91a99e8d72bd6eb76999325bf7548543d960f
williamndongmo88 <williamndongmo88@gmail.com>
2025-09-19 17:40:00 +0200

    Add railway deployment config

--- src/main/java/will/dev/qrcodeApp/SolSolutionQrCodeAppApplication.java
+++ src/main/java/will/dev/qrcodeApp/SolSolutionQrCodeAppApplication.java
@@ -1,5 +1,6 @@
 package will.dev.qrcodeApp;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -24,5 +25,16 @@ public class SolSolutionQrCodeAppApplication {
                         .allowedHeaders("*");
             }
         };
+    }
+
+    @PostConstruct
+    public void testDBEnvVars() {
+        System.out.println("===================================");
+        System.out.println("DB URL: " + System.getProperty("DATABASE_URL"));
+        System.out.println("DB USER: " + System.getProperty("DATABASE_USERNAME"));
+        System.out.println("MAIL_USERNAME = " + System.getProperty("MAIL_USERNAME"));
+        System.out.println("👉 lienDuSite: " + System.getProperty("API_URL"));
+        System.out.println("👉 BaseUrl: " + System.getProperty("BASE_URL"));
+        System.out.println("===================================");
     }
 }
